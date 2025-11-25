c060e56a7f5153bc702b03f03fd2c90d2f1f1c93
CithaAu002 <cithanuest748@gmail.com>
2025-11-25 21:15:22 +0700

    ubah nama role

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('donatur'); 
+            $table->string('role')->default('user'); 
             $table->rememberToken();
             $table->timestamps();
         });
