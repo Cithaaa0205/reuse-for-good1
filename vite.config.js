b7c9b0f9ffcba1fa9cef812a9f3709560c18f83e
Aldo Sepvenco <aldosepvenco@gmail.com>
2025-12-18 03:18:37 +0700

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,38 @@
-// // vite.config.js
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-// import tailwindcss from '@tailwindcss/vite';
-
-// export default defineConfig({
-//     server: {
-//         host: true,        // Biar bisa diakses dari device lain di jaringan yang sama
-//         port: 5173,        // Port default Vite
-//         hmr: {
-//             host: '10.200.25.193', // GANTI dengan IP laptop kamu di WiFi kampus/rumah
-//         },
-//     },
-//     plugins: [
-//         laravel({
-//             input: [
-//                 'resources/css/app.css',
-//                 'resources/js/app.js',
-//             ],
-//             refresh: true,
-//         }),
-//         tailwindcss(),
-//     ],
-// });
-
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: true,        // Biar bisa diakses dari device lain di jaringan yang sama
+        port: 5173,        // Port default Vite
+        hmr: {
+            host: '10.30.183.206', // GANTI dengan IP laptop kamu di WiFi kampus/rumah
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
 });
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+// });
